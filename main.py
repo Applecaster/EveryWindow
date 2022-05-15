6e9e0a60f6b805bbf8c35d3d4c4210780558a898
Gadi Bessudo <gadibessudo@gmail.com>
2022-05-16 00:23:05 +0300

    update on every keystroke

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def callback(event):
     print("You pressed Enter")
     print(event)
 
-def cb_search(event):
+def cb_searchx(*args):
     global g_listbox
     global g_search_str
     
@@ -40,7 +40,7 @@ def cb_search(event):
         if item.lower().find(sstr.lower())>=0:
             filtered_data.append(item)
   
-    fill_listbox(filtered_data)   
+    fill_listbox(filtered_data)  
 
 
 def fill_listbox(ld):
@@ -56,10 +56,10 @@ def main():
     #app.bind('<Return>', callback)
 
     g_search_str = tkinter.StringVar()
+    g_search_str.trace("w", cb_searchx)
     
     search_entry = tkinter.Entry(app, textvariable=g_search_str, width=10)
     search_entry.pack()
-    search_entry.bind('<Return>', cb_search)
     search_entry.focus_set()
 
     g_listbox = tkinter.Listbox(app)
